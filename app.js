cb9f9af5b08661d623589a7ae11973eb1f2e95d4
Dylan <dylanchurchward710@gmail.com>
2022-08-24 11:18:17 -0700

    Copied several microservices from another one of my projects, in order to host them remotely

--- app.js
+++ app.js
@@ -1,38 +1,54 @@
-// const app = require("express")();
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+var express = require("express");
+var app = express();
 
-app.use(express.json());
+const PORT = process.env.PORT || 3000
 
 
-app.get("/", (req, res) => {
-    res.send("Hello world");
+// convert celcius to fahrenheit microservice
+app.get('/celciusToFahrenheit/:num', function(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*"); // this line allows the local server to work properly while using "live server"
+    var result = (Number(req.params.num) * 1.8) + 32
+    res.json({"result": result});
 });
 
-// test method
-app.post('/tshirt/:id', (req, res) => {
-
-    const { id } = req.params;
-    const { logo } = req.body;
-
-    if (!logo) {
-        res.status(418).send({ message: 'We need a logo!' });
-    } else {
-        res.send({tshirt: `Tshirt with your ${logo} and ID of ${id}`});
-    }
-
+// convert fahrenheit to celcius microservice
+app.get('/fahrenheitToCelcius/:num', function(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*"); // this line allows the local server to work properly while using "live server"
+    var result = (Number(req.params.num) - 32) * (5/9)
+    res.json({"result": result});
 });
 
-// test method
-app.get('/tshirt', (req, res) => {
-    res.status(200).send({
-        tshirt: `I'm a shirt`,
-        size: `large`
-    });
+// convert centimeters to inches microservice
+app.get('/centimetersToInches/:num', function(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*"); // this line allows the local server to work properly while using "live server"
+    var result = Number(req.params.num) / 2.54
+    res.json({"result": result});
 });
 
-// Fire up the server
-app.listen(PORT, () => {
-    console.log(`App up at port ${PORT}`);
+// convert inches to centimeters microservice
+app.get('/inchesToCentimeters/:num', function(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*"); // this line allows the local server to work properly while using "live server"
+    var result = Number(req.params.num) * 2.54
+    res.json({"result": result});
 });
+
+// convert pounds to kilograms microservice
+app.get('/poundsToKilograms/:num', function(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*"); // this line allows the local server to work properly while using "live server"
+    var result = Number(req.params.num) * 0.45359237
+    res.json({"result": result});
+});
+
+// convert kilograms to pounds microservice
+app.get('/kilogramsToPounds/:num', function(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*"); // this line allows the local server to work properly while using "live server"
+    var result = Number(req.params.num) * 2.2
+    res.json({"result": result});
+});
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server RUNNNNNNING on port ${process.env.PORT}`);
+});
+
+
+
