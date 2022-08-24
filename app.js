6275dc76fbedee9ff6a42893ee6904a34a466508
Dylan <dylanchurchward710@gmail.com>
2022-08-24 09:38:12 -0700

    added another get method for testing purposes

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
     res.send("Hello world");
 });
 
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: `I'm a shirt`,
+        size: `large`
+    });
+});
+
+// Fire up the server
 app.listen(PORT, () => {
     console.log(`App up at port ${PORT}`);
 });
