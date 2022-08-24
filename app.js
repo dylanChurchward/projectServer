4754f1e00996dd410529520bd091cc497698be6e
Dylan <dylanchurchward710@gmail.com>
2022-08-24 09:48:39 -0700

    added express middleware for parsing json, added a couple test API end points

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const app = require("express")();
-
+const exp = express(); // middleware for parsing JSON 
 const PORT = process.env.PORT || 3000;
+
+exp.use(express.json()); // use the middle for parsing JSON 
+
 
 app.get("/", (req, res) => {
     res.send("Hello world");
 });
 
+// test method
+app.post('/tshirt/:id', (req, res) => {
+
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({ message: 'We need a logo!' });
+    } else {
+        res.send({tshirt: `Tshirt with your ${logo} and ID of ${id}`});
+    }
+});
+
+// test method
 app.get('/tshirt', (req, res) => {
     res.status(200).send({
         tshirt: `I'm a shirt`,
