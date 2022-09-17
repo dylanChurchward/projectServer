9e0ed2623ca58c7b4d1bd7ac9ac3e6ec3e0a2f3e
Dylan <dylanchurchward710@gmail.com>
2022-09-17 14:34:30 -0700

    trouble shooting new api endpoint

--- app.js
+++ app.js
@@ -87,13 +87,16 @@ app.get('/getLeaderboard', function(req, res) {
 
 
     // Add a new record to leader board database 
-app.get('/putLeaderboard/:playername/:score', function(req, res) {
+app.get('/putLeaderboard/:playername/', function(req, res) {
     res.setHeader("Access-Control-Allow-Origin", "*"); // this line allows the local server to work properly while using "live server"
 
-    client.query(`INSERT INTO leaderboard(playerName, score) VALUES (${playername}, ${score}})`, (err, response) => {
-        res.json(response.rows);
-        client.end;
-    })
+    // client.query(`INSERT INTO leaderboard(playerName, score) VALUES (${playername}, ${score}})`, (err, response) => {
+    //     res.json(response.rows);
+    //     client.end;
+    // })
+
+        res.json({"ello": "mate"});
+
 
 });
 });
