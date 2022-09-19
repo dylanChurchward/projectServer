359eef6b4f031426ece793ef4511bd0cb3c0de9c
Dylan <dylanchurchward710@gmail.com>
2022-09-18 20:27:19 -0700

    new comment

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.get('/getLeaderboard/:limit', function(req, res) {
 
 });
 
-// Add a new record to leader board database 
+// Add a new record to leader board database, return updated sorted list of leader board
 app.get('/putLeaderboard/:playername/:score', function (req, res) {
     res.setHeader("Access-Control-Allow-Origin", "*"); // this line allows the local server to work properly while using "live server"
 
